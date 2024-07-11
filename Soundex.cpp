85f284bcd92fa6052398f9a2b81c1ed46520e86f
nitish1703-sys <nitish.tanguturi@gmail.com>
2024-07-11 11:47:23 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,85 +1,51 @@
 #include "Soundex.h"
 #include <cctype>
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <vector>
+#include <unordered_map>
  
-std::string target;
-std::string removeChars(std::string s, int pos, bool f( char c ) )
-{
-   if ( pos >= s.size() ) 
-   {
-       return s; 
-   }
-   std::string::iterator end = remove_if( s.begin() + pos, s.end(), f );  
-   return s.substr( 0, end - s.begin() );                            
+static const std::unordered_map<char, char> soundexCodes {
+    {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+    {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'},
+    {'S', '2'}, {'X', '2'}, {'Z', '2'},
+    {'D', '3'}, {'T', '3'},
+    {'L', '4'},
+    {'M', '5'}, {'N', '5'},
+    {'R', '6'}
+};
+ 
+char getSoundexCode(char c) {
+    c = std::toupper(c);
+    auto it = soundexCodes.find(c);
+    return (it != soundexCodes.end()) ? it->second : '0';
 }
  
-bool isIn( char c ) 
-{ 
-    return target.find( toupper( c ) ) != std::string::npos; 
-}
- 
-bool notAlpha( char c ) 
-{ 
-    return !isalpha( c ); 
-}
- 
-std::string generateSoundex(const std::string& name)
-{
-    if (name.empty()) 
-    {
-        return "";
+void appendSoundexCode(std::string& soundex, char code, char& prevCode) {
+    if (code != '0' && code != prevCode) {
+        soundex += code;
+        prevCode = code;
     }
-    
-   std::map<char,char> cmap = 
-   { 
-        {'B','1'}, {'F','1'}, {'P','1'}, {'V','1'},
-        
-        {'C','2'}, {'G','2'}, {'J','2'}, {'K','2'}, {'Q','2'}, {'S','2'}, {'X','2'}, {'Z','2'},
-        
-        {'D','3'}, {'T','3'},
-        
-        {'L','4'},
-        
-        {'M','5'}, {'N','5'},
-        
-        {'R','6'},
-    };
+}
  
-   std::string result = removeChars( name, 0, notAlpha );
-   for ( char &c : result ) 
-   {
-        c = toupper( c ); 
-   }
+std::string processInitialCharacter(const std::string& name) {
+    std::string soundex;
+    soundex += std::toupper(name[0]);
+    return soundex;
+}
  
-   char firstLetter = result[0];
-   target = "HW";   
-   result = removeChars( result, 1, isIn );
+void processRemainingCharacters(const std::string& name, std::string& soundex) {
+    char prevCode = getSoundexCode(name[0]);
  
-   for ( char &c : result ) 
-   { 
-       if ( cmap.count( c ) )
-       {
-           c = cmap[c]; 
-       }
-   }
+    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
+        char code = getSoundexCode(name[i]);
+        appendSoundexCode(soundex, code, prevCode);
+    }
+}
  
-   for ( int i = 1; i < result.size(); i++ ) 
-   {
-       if ( result[i] == result[i-1] )
-       {
-           result[i-1] = '*';
-       }
-   }
+std::string generateSoundex(const std::string& name) {
+    if (name.empty()) return "";
  
-   target = "*"; 
-   result = removeChars( result, 1, isIn );
-   target = "AEIOUY";   
-   result = removeChars( result, 1, isIn );
-   result[0] = firstLetter;
-   result += "000";
-   return result.substr( 0, 4 );
+    std::string soundex = processInitialCharacter(name);
+    processRemainingCharacters(name, soundex);
+ 
+    soundex.resize(4, '0');
+    return soundex;
 }
